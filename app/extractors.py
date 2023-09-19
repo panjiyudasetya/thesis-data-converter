157f70b10dd6fcf5d53fdd8c05a33160d38b5502
panjiyudasetya <panjiyudasetya@gmail.com>
2023-09-19 11:34:03 +0700

    Add communication dataframe retrieval

--- app/extractors.py
+++ app/extractors.py
@@ -58,10 +58,36 @@ class ClientProfile:
             dtype={
                 'client_id': str,
                 'therapist_id': str,
-                'no_of_days_with_calls': 'Int64',
-                'no_of_registrations': 'Int64',
+                'no_of_days_with_calls': 'int64',
+                'no_of_registrations': 'int64',
                 'start_time': str,
                 'end_time': str,
             },
             parse_dates=['start_time', 'end_time']
+        )
+
+
+class Communication:
+
+    def select(self, snapshot_date: date) -> pd.DataFrame:
+        """
+        Selects snapshot of the communication data from local storage
+        that have been downloaded at the given `snapshot_date`.
+        """
+        snapshot_date_str = snapshot_date.strftime("%Y-%m-%d")
+
+        directory = f'{FILE_LOCATOR.communications[FILE_LOCATOR.DIR]}'
+        filename = f'{FILE_LOCATOR.communications[FILE_LOCATOR.FILENAME]}'
+
+        path = f'{directory}/{snapshot_date_str}/{filename}'
+
+        return pd.read_csv(
+            path,
+            dtype={
+                'client_id': str,
+                'start_time': str,
+                'call_made': bool,
+                'chat_msg_sent': bool,
+            },
+            parse_dates=['start_time']
         )
