a672980102e3b82ae440ab22ccb54df04fc56fbd
panjiyudasetya <panjiyudasetya@gmail.com>
2023-09-19 13:07:28 +0700

    Add thought record dataframe retrieval

--- app/extractors.py
+++ app/extractors.py
@@ -256,3 +256,33 @@ class TherapySession:
             },
             parse_dates=['start_time']
         )
+
+
+class ThoughtRecord:
+
+    def select(self, snapshot_date: date) -> pd.DataFrame:
+        """
+        Selects snapshot of the thought records data from local storage
+        that have been downloaded at the given `snapshot_date`.
+        """
+        snapshot_date_str = snapshot_date.strftime("%Y-%m-%d")
+
+        directory = f'{FILE_LOCATOR.thought_records[FILE_LOCATOR.DIR]}'
+        filename = f'{FILE_LOCATOR.thought_records[FILE_LOCATOR.FILENAME]}'
+
+        path = f'{directory}/{snapshot_date_str}/{filename}'
+
+        # Read dataframe
+        df = pd.read_csv(
+            path,
+            dtype={
+                'client_id': str,
+                'start_time': str,
+            }
+        )
+
+        # The `start_time` column is a stringify date-time, 
+        # and `parse_dates` property can't convert them automatically.
+        # Therefore, we need to convert it to datetime object manually.
+        df['start_time'] = pd.to_datetime(df['start_time'], format='ISO8601')
+        return df
