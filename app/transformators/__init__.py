0bf4b47d963e4413683b870d79aa01ed5c84ed29
panjiyudasetya <panjiyudasetya@gmail.com>
2023-09-20 17:45:54 +0700

    Add diary entries to criterion transformator

--- app/transformators/__init__.py
+++ app/transformators/__init__.py
@@ -1,4 +1,5 @@
 from app.transformators.calls_to_treatment_phase import calls_to_treatment_phase
+from app.transformators.diary_entries_to_criterion import diary_entries_to_criterion
 from app.transformators.interactions_to_criterion import interactions_to_criterion
 from app.transformators.negative_registrations_to_criterion import negative_registrations_to_criterion
 from app.transformators.planned_events_to_criterion import planned_events_to_criterion
@@ -9,6 +10,7 @@ from app.transformators.thought_records_to_criterion import thought_records_to_c
 
 __all__ = [
     'calls_to_treatment_phase',
+    'diary_entries_to_criterion',
     'interactions_to_criterion',
     'negative_registrations_to_criterion',
     'planned_events_to_criterion',
