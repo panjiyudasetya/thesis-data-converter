bb33ac616067be4e2e2ba326eaf2948e9dc7a9dd
panjiyudasetya <panjiyudasetya@gmail.com>
2023-09-21 11:47:28 +0700

    Update converter `main` code

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 from app.extractors import MetabaseCollection
 from app.loaders import Criteria
-from app.settings import app_settings as settings
+
 
 if __name__ == '__main__':
     # Extracts all collections from Metabase
-    MetabaseCollection().extract_all()
+    MetabaseCollection().collect_all()
 
-    # Loads criteria data for specific date
-    for_date = settings.running_date()
-    Criteria(for_date).load()
+    # Loads criteria data
+    Criteria().load()
