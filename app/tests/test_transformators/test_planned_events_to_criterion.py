3123f005d272e3dd7889e90920b414e19823c264
panjiyudasetya <panjiyudasetya@gmail.com>
2023-10-04 19:37:15 +0700

    Add unit tests for planned events to criterion transformator

---
+++ app/tests/test_transformators/test_planned_events_to_criterion.py
@@ -0,0 +1,78 @@
+import pandas as pd
+
+from dateutil.parser import parse
+from unittest import TestCase
+
+from app.transformators.planned_events_to_criterion import (
+    planned_events_to_criterion,
+)
+
+
+class TestPlannedEventsToCriterion(TestCase):
+    """
+    Test the `planned_events_to_criterion` transformator.
+    """
+
+    def test_planned_events_to_criterion_1(self):
+        """
+        Test to ensure the `planned_events_to_criterion` method
+        returns correct criterion.
+        """
+        events_completion = pd.DataFrame(data={
+            'client_id': [],
+            'planned_event_id': [],
+            'start_time': [],
+            'status': []
+        })
+
+        actual = planned_events_to_criterion(events_completion)
+        expected = 3  # Unplanned
+        self.assertEqual(actual, expected)
+
+    def test_planned_events_to_criterion_2(self):
+        """
+        Test to ensure the `planned_events_to_criterion` method
+        returns correct criterion.
+        """
+        events_completion = pd.DataFrame(data={
+            'client_id': ['cid-1'],
+            'planned_event_id': ['pid-1'],
+            'start_time': [parse('2023-09-01')],
+            'status': ['COMPLETED']
+        })
+
+        actual = planned_events_to_criterion(events_completion)
+        expected = 1  # Complete
+        self.assertEqual(actual, expected)
+
+    def test_planned_events_to_criterion_3(self):
+        """
+        Test to ensure the `planned_events_to_criterion` method
+        returns correct criterion.
+        """
+        events_completion = pd.DataFrame(data={
+            'client_id': ['cid-1'],
+            'planned_event_id': ['pid-1'],
+            'start_time': [parse('2023-09-01')],
+            'status': ['INCOMPLETED']
+        })
+
+        actual = planned_events_to_criterion(events_completion)
+        expected = 0  # Incomplete
+        self.assertEqual(actual, expected)
+
+    def test_planned_events_to_criterion_4(self):
+        """
+        Test to ensure the `planned_events_to_criterion` method
+        returns correct criterion.
+        """
+        events_completion = pd.DataFrame(data={
+            'client_id': ['cid-1', 'cid-1'],
+            'planned_event_id': ['pid-1', 'pid-2'],
+            'start_time': [parse('2023-09-01'), parse('2023-09-02')],
+            'status': ['COMPLETED', 'INCOMPLETED']
+        })
+
+        actual = planned_events_to_criterion(events_completion)
+        expected = 2  # Some complete
+        self.assertEqual(actual, expected)
