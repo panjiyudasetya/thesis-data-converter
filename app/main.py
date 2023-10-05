9b2080cfcd54c204479c8e1fc9fb6d1108c4efb2
panjiyudasetya <panjiyudasetya@gmail.com>
2023-10-05 09:30:17 +0700

    Update main code

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from app.extractors import MetabaseCollection
 from app.loaders import Criteria
+from app.settings import app_settings as settings
 
 
 if __name__ == '__main__':
     # Extracts all collections from Metabase
-    MetabaseCollection().download()
+    # when necessary.
+    if settings.USE_REMOTE_DATA:
+        MetabaseCollection().download()
 
     # Loads criteria data
     Criteria().load()
