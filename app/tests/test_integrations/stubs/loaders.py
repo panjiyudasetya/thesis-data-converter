33f9efc8d548559c0ef01d317176b4d36db18c05
panjiyudasetya <panjiyudasetya@gmail.com>
2023-11-10 17:50:04 +0700

    Add stubs for the `loaders`

---
+++ app/tests/test_integrations/stubs/loaders.py
@@ -0,0 +1,10 @@
+
+class StubCriteria:
+
+    def load(self) -> None:
+        """
+        Stub interface for creating criteria data of the clients
+        who has social anxiety disorder,
+        and then stores them either to remote database or local storage.
+        """
+        print('Stub `loaders.Criteria.load` is called.')
